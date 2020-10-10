f2b032db3decbacca4fdad2e8797c04e7ebbba0a
Soundararajan M <soundarit1995@gmail.com>
2020-10-10 15:42:09 +0530

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateComponent } from './create/create.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'create', component: CreateComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateComponent } from './create/create.component';
+import { DashComponent } from './dash/dash.component';
+import { IssueComponent } from './issue/issue.component';
+import { IssuesComponent } from './issue/issues/issues.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CreateComponent
+    CreateComponent,
+    DashComponent,
+    IssueComponent,
+    IssuesComponent
   ],
   imports: [
     BrowserModule,
