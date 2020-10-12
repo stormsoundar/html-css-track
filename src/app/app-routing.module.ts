f323572ea7dcd64173f5b4ed6fd415df4d9cead6
Soundararajan M <soundarit1995@gmail.com>
2020-10-12 08:37:19 +0530

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateComponent } from './create/create.component';
+import { DashComponent } from './dash/dash.component';
+import { IssueComponent } from './issue/issue.component';
+import { IssuesComponent } from './issue/issues/issues.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashComponent },
+  { path: 'create', component: CreateComponent },
+  { path: 'issue', component: IssueComponent },
+  { path: 'issue/issues', component: IssuesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/dash/dash.component.ts
@@ -0,0 +1,216 @@
+import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:4200');
+
+@Component({
+  selector: 'app-dash',
+  templateUrl: './dash.component.html',
+  styleUrls: ['./dash.component.css']
+})
+export class DashComponent implements OnInit {
+
+  title = 'dashboard';
+  chart;
+  chart2 = [];
+  pie: any;
+  doughnut: any;
+
+  data1 = [];
+
+  ngOnInit() {
+
+    socket.on('data1', (res) => {
+      this.updateChartData(this.chart, res, 0);
+      this.updateChartData(this.doughnut,res.slice(0,5), 0);
+    })
+
+    socket.on('data2', (res) => {
+      this.updateChartData(this.chart, res, 1);
+    })
+
+    this.chart = new Chart('bar', {
+      type: 'bar',
+      options: {
+        responsive: true,
+        title: {
+          display: true,
+          text: 'Combo Bar and line Chart'
+        },
+      },
+      data: {
+        labels: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'],
+        datasets: [
+          {
+            type: 'bar',
+            label: 'My First dataset',
+            data: [243, 156, 365, 30, 156, 265, 356, 543],
+            backgroundColor: 'rgba(255,0,255,0.4)',
+            borderColor: 'rgba(255,0,255,0.4)',
+            fill: false,
+          },   
+          {
+            type: 'bar',
+            label: 'My Second dataset',
+            data: [243, 156, 365, 30, 156, 265, 356, 543].reverse(),
+            backgroundColor: 'rgba(0,0,255,0.4)',
+            borderColor: 'rgba(0,0,255,0.4)',
+            fill: false,
+          }
+        ]
+      }
+    });
+
+    let options = {
+      tooltips: false,
+
+      elements: {
+        point: {
+          borderWidth: function (context) {
+            return Math.min(Math.max(1, context.datasetIndex + 1), 8);
+          },
+          hoverBackgroundColor: 'transparent',
+          hoverBorderColor: function (context) {
+            return "red";
+          },
+          hoverBorderWidth: function (context) {
+            var value = context.dataset.data[context.dataIndex];
+            return Math.round(8 * value.v / 1000);
+          },
+          radius: function (context) {
+            var value = context.dataset.data[context.dataIndex];
+            var size = context.chart.width;
+            var base = Math.abs(value.v) / 1000;
+            return (size / 24) * base;
+          }
+        }
+      }
+    };
+
+
+    new Chart('bubble', {
+      type: 'bubble',
+      options: options,
+      data: {
+        datasets: [
+          {
+          backgroundColor: 'rgba(255,0,0,0.4)',
+          label: 'Name 1',
+          data: [{
+            x: 50,
+            y: 60,
+            v: 200
+            },{
+              x: 50,
+              y: 80,
+              v: 700
+            },{
+              x: 80,
+              y: 60,
+              v: 100
+            },{
+            x: 60,
+            y: 60,
+            v: 500
+            },{
+            x: 90,
+            y: 80,
+            v: 800
+            }]
+          },{
+            backgroundColor: 'rgba(0,255,0,0.4)',
+            label: 'Name 2',
+            data: [{
+              x: 60,
+              y: 20,
+              v: 200
+            },{
+              x: 55,
+              y: 70,
+              v: 800
+            }, {
+              x: 80,
+              y: 30,
+              v: 500
+            },{
+              x: 70,
+              y: 40,
+              v: 800
+            }]}]
+      }
+    })
+
+    this.doughnut =  new Chart('doughnut',{
+      type: 'doughnut',
+      options: {
+        responsive: true,
+        title: {
+          display: true,
+          text: 'Doughnut Chart'
+        },legend: {
+					position: 'top',
+				},animation: {
+					animateScale: true,
+					animateRotate: true
+				}
+      },
+      data: {
+				datasets: [{
+					data: [45,10,5,25,15],
+					backgroundColor: ["red","orange","yellow","green","blue"],
+					label: 'Dataset 1'
+				}],
+				labels: ['Red','Orange','Yellow','Green','Blue']
+			}
+    })
+
+    this.pie = new Chart('pie',{
+      type: 'pie',
+      options: {
+        responsive: true,
+        title: {
+          display: true,
+          text: 'Pie Chart'
+        },legend: {
+					position: 'top',
+				},animation: {
+					animateScale: true,
+					animateRotate: true
+				}
+      },
+      data: {
+				datasets: [{
+					data: [45,10,5,25,15].reverse(),
+					backgroundColor: ["red","orange","yellow","green","blue"],
+					label: 'Dataset 1'
+				}],
+				labels: ['Red','Orange','Yellow','Green','Blue']
+			}
+    })
+
+  }
+
+  addData(chart, label, data) {
+    chart.data.labels.push(label);
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+    });
+    chart.update();
+}
+
+removeData(chart) {
+    chart.data.labels.pop();
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.pop();
+    });
+    chart.update();
+}
+
+updateChartData(chart, data, dataSetIndex){
+  chart.data.datasets[dataSetIndex].data = data;
+  chart.update();
+}
+
+
+}
